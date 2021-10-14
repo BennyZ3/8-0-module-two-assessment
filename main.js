e906b73622a2a8f0780146ddc56449e560d8398a
BennyZ3 <bennyz3@hotmail.com>
2021-10-14 12:39:53 -0400

    cleaned up logs, rearranged queries, added comments

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
+//Selectors
 const movieSelector = document.querySelector("#movie_selector");
-// console.log(movieSelector);
+const movieTitle = document.querySelector(".movie_title");
+const movieYear = document.querySelector(".movie_year");
+const movieDescription = document.querySelector(".movie_description");
+const reviewList = document.querySelector(".review_list");
+
 let moviesObject = [];
+//api call
 fetch("https://ghibliapi.herokuapp.com/Films").then((response) =>
   response.json().then((object) => {
-    // console.log(object);
     moviesObject = object;
     let count = 0;
     object.forEach((movie) => {
-      //   console.log(movie.title);
       let newOption = document.createElement("option");
       newOption.value = count;
       count++;
       newOption.textContent = movie.title;
-      //   console.log(newOption);
       movieSelector.appendChild(newOption);
     });
   })
 );
 
-const movieTitle = document.querySelector(".movie_title");
-const movieYear = document.querySelector(".movie_year");
-const movieDescription = document.querySelector(".movie_description");
+//adjusting text content based on selected movie
 movieSelector.addEventListener("change", (event) => {
+  //issue: won't display first item contents without going to another first
   let movie = moviesObject[event.target.value];
   movieTitle.textContent = movie.title;
   movieYear.textContent = movie.release_date;
   movieDescription.textContent = movie.description;
 });
 
-const reviewList = document.querySelector(".review_list");
+//review submission
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
   if (event.target.review.value) {
